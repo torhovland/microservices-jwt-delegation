d1fbd3a4cecfc5e6a77b47dc013e26482e4d916d
Tor Hovland <tor+gitextensions@hovland.co>
2017-04-08 19:20:45 +0200

    Made service use JWT bearer auth.

--- ServiceB/Controllers/ValuesController.cs
+++ ServiceB/Controllers/ValuesController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ServiceB.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
@@ -13,32 +15,11 @@ namespace ServiceB.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var user = HttpContext.User;
+            var userId = user.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var displayName = user.Claims.FirstOrDefault(c => c.Type == "name")?.Value;
+            return new[] { $"Service B has recognized you as {displayName} with identity {userId}." };
         }
     }
 }
+
